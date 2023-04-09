658e045179d9a20ee5264511c06860f1c1c297d9
e.ternovskiy <ternovskiy.egor555@gmail.com>
2023-04-09 09:38:32 +0300

    Full functionality added

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 let vouchers = [];
+let totalPrice = 0;
+let ID = 0;
 
 let myVouchers = document.getElementsByClassName('my__vouchers')[0];
 
@@ -15,21 +17,73 @@ createVoucher.onclick = function() {
 myVouchers.onclick = function() {
     if (!myVouchers.classList.contains('navbar__selected')) {
         togglerNavbar();
-        printVouchers();
+        printVouchers(vouchers);
     }
 }
 
 let formCreateVoucher = document.forms.create__voucher__form;
 formCreateVoucher.onsubmit = function(e) {
-    e.preventDefault();                 
-    let voucher = {
-        country: this.country.value,
-        description: this.description.value,
-        days: this.days.value,
-        price: this.price.value
+    e.preventDefault();
+    if (this.country.value === "" ||
+        this.description.value === "" ||
+        this.days.value === "" ||
+        this.price.value === "") {
+            let alert = document.getElementById('alertMessage');
+            alert.classList.add('show__alertMessage');
+        setTimeout(() => {
+            alert.classList.remove('show__alertMessage');
+        }, 3000);
+        let closeAlertButton = document.getElementsByClassName('close__button')[0];
+            closeAlertButton.onclick = function() {
+                alert.classList.remove('show__alertMessage');
+            }
+    } else {
+        let voucher = {
+            id: ++ID,
+            country: this.country.value,
+            description: this.description.value,
+            days: this.days.value,
+            price: this.price.value
+        }
+        vouchers.push(voucher);
+        totalPrice += +voucher.price;
+        this.reset();
     }
-    vouchers.push(voucher);
-    this.reset();
+}
+
+let btnSearch = document.getElementById('find_search');
+btnSearch.onclick = function() {
+    let input = document.getElementById('finder');
+    let findValue = input.value;
+    printVouchers(vouchers.filter(voucher => {
+        return voucher.country.includes(findValue) || 
+            voucher.description.includes(findValue);
+    })); 
+}
+
+let btnClearSearch = document.getElementById('find_clear');
+btnClearSearch.onclick = function() {
+    let input = document.getElementById('finder');
+    input.value = '';
+    printVouchers(vouchers);
+}
+
+let switcher = document.getElementsByClassName('radioButton')[0];
+let isSelected = false;
+switcher.onclick = function() {
+    switcher.classList.toggle('radioButton__selected')
+    if (!isSelected) {
+        printVouchers(vouchers.sort((a, b) => b.price - a.price));
+    } else {
+        printVouchers(vouchers.sort((a, b) => a.id - b.id));
+    }
+    isSelected = !isSelected;
+}
+
+let buttonCountPrice = document.getElementById('countTotalPrice');
+buttonCountPrice.onclick = function() {
+    let counter = document.getElementsByClassName('totalPriceOutput')[0];
+    counter.innerText = `${totalPrice} €`;
 }
 
 function togglerNavbar() {
@@ -39,9 +93,34 @@ function togglerNavbar() {
     vouchersContainer.classList.toggle('hide__vouchers__container');
 }
 
-function printVouchers() {
+function changeInfo(voucher) {
+    let editDiv = document.getElementsByClassName('edit__voucher')[0];
+    let editForm = document.forms.editVoucher;
+    editForm.country.value = voucher.country;
+    editForm.description.value = voucher.description;
+    editForm.days.value = voucher.days;
+    editForm.price.value = voucher.price;
+    editDiv.classList.toggle('show__edit__voucher');
+    editForm.saveButton.onclick = function(e) {
+        e.preventDefault();
+        vouchers[vouchers.indexOf(voucher)] = {
+            country: editForm.country.value,
+            description: editForm.description.value,
+            days: editForm.days.value,
+            price: editForm.price.value
+        };
+        editDiv.classList.toggle('show__edit__voucher');
+        printVouchers(vouchers);
+    }
+    editForm.closeButton.onclick = function(e) {
+        e.preventDefault();
+        editDiv.classList.toggle('show__edit__voucher');
+    }
+}
+
+function printVouchers(vouchers) {
     vouchersContainer.innerHTML = '';
-        for (const voucher of vouchers) {
+        for (let voucher of vouchers) {
             let voucherDiv = document.createElement('div');
             voucherDiv.classList.add('voucher');
             let voucherImgDiv = document.createElement('div');
@@ -70,13 +149,14 @@ function printVouchers() {
             btnRemove.innerText = 'Remove';
             btnEdit.setAttribute('id', "edit_voucher");
             btnEdit.onclick = function() {
-                changeInfo();
-                printVouchers();
+                changeInfo(voucher);
             }
             btnRemove.setAttribute('id', "remove_voucher");
             btnRemove.onclick = function() {
+                console.log(vouchers);
+                totalPrice -= +voucher.price;
                 vouchers.splice(vouchers.indexOf(voucher), 1);
-                printVouchers();
+                printVouchers(vouchers);
             }
             btnGroup.appendChild(btnEdit);
             btnGroup.appendChild(btnRemove);
